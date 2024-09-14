cd7d3cfe20d658344c787db7e92f199b88a309a0
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-14 13:35:23 +0530

    Update an existing record using put method.

--- http_put_delete/src/main/java/com/example/http_put_delete/controller/StudentController.java
+++ http_put_delete/src/main/java/com/example/http_put_delete/controller/StudentController.java
@@ -1,5 +1,49 @@
 package com.example.http_put_delete.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.http_put_delete.model.Student;
+import com.example.http_put_delete.service.StudentService;
+
+
+@RestController
 public class StudentController {
 
+	@Autowired
+	StudentService service;
+	
+	@RequestMapping("/")
+	public String home() {
+		return "This is a Home Page of our Project";
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getAllStudents(){
+		return service.getAllStudents();
+	}
+	
+	@GetMapping("/students/{id}")
+	public Student getStudentById(@PathVariable int id) {
+		return service.getStudentById(id);
+	}
+	
+	@PostMapping("/students")
+	public void addStudent(@RequestBody Student student) {
+		service.addStudent(student);
+	}
+	
+	@PutMapping("/students/{id}")
+	public Student updateStudent(@PathVariable int id, @RequestBody Student student) {
+		return service.updateStudent(id,student);
+	}
+	
 }
--- http_put_delete/src/main/java/com/example/http_put_delete/service/StudentService.java
+++ http_put_delete/src/main/java/com/example/http_put_delete/service/StudentService.java
@@ -1,5 +1,43 @@
 package com.example.http_put_delete.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.example.http_put_delete.model.Student;
+
+
+@Service
 public class StudentService {
+
+	List<Student> students = new ArrayList(Arrays.asList(new Student(101,"Tejas",89),
+			new Student(102,"Shubham",90)));
+	
+	public List<Student> getAllStudents() {
+		return students;
+	}
+
+	public Student getStudentById(int id) {
+		return students.stream()
+				.filter(p -> p.getId() == id)
+				.findFirst().orElse(new Student());
+		}
+
+	public void addStudent(Student student) {
+		students.add(student);
+	}
+
+	public Student updateStudent(int id,Student student) {
+		Student s = students.stream()
+				.filter(p -> p.getId() == id)
+				.findFirst().get();		
+		int index = students.indexOf(s);
+		students.set(index, student);
+		return student;
+	}
 
 }
