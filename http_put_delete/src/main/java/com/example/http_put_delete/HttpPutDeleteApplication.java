a67a00d7284e2526cd1f31dd0463af5ec80f66a3
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-14 13:15:41 +0530

    Initial Commit

---
+++ http_put_delete/src/main/java/com/example/http_put_delete/HttpPutDeleteApplication.java
@@ -0,0 +1,13 @@
+package com.example.http_put_delete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpPutDeleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpPutDeleteApplication.class, args);
+	}
+
+}
---
+++ http_put_delete/src/test/java/com/example/http_put_delete/HttpPutDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.http_put_delete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpPutDeleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
