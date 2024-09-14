e5091cbb399b399c1504b80c7f8e63ab387e2ae3
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-14 12:07:19 +0530

    Initial Commit

---
+++ http_get_post/src/main/java/com/example/http_get_post/HttpGetPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.http_get_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpGetPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpGetPostApplication.class, args);
+	}
+
+}
---
+++ http_get_post/src/test/java/com/example/http_get_post/HttpGetPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.http_get_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpGetPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
